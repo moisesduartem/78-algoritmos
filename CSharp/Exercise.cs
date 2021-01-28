39d3fea79cd879b3f3ee0c37e00e36dd16b7883f
Moisés Mariano <moises@softbuilder.com.br>
2021-01-28 09:05:07 -0300

    feat: create the initial structure to execute the exercises

---
+++ CSharp/Exercise.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharp
+{
+    public class Exercise
+    {
+        public static void One()
+        {
+            Console.WriteLine("I'm the exercise one!");
+        }
+    }
+}
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,12 +1,50 @@
-﻿using System;
+﻿using System.Reflection;
+using System;
 
 namespace CSharp
 {
     class Program
     {
+        private static string[] exercises = {
+            "One"
+        };
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            start();
+        }
+
+        private static void start()
+        {
+            try
+            {
+                Console.Write("Execute o exercício desejado: ");
+                int exerciseArrayIndex = int.Parse(Console.ReadLine());
+                ExecuteExercise(exerciseArrayIndex);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void ExecuteExercise(int exerciseNumber)
+        {
+            try
+            {
+                Type type = typeof(Exercise);
+                MethodInfo method = type.GetMethod(exercises[exerciseNumber]);
+                method.Invoke(ExerciseContainer(), null);
+            }
+            catch (Exception e)
+            {
+                throw new Exception("Não existe um exercício com o índice digitado.");
+            }
+        }
+
+        private static Exercise ExerciseContainer()
+        {
+            return new Exercise();
         }
     }
 }
